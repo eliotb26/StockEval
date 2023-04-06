f75d7ec10878eee92c5ee76490a4d4888bb13e8b
Eliot <eliotb26@gmail.com>
2023-04-06 01:51:24 -0400

    Added annualized_volatility, sharpe_ratio, and expected returns to MonteCarlo

--- monteCarlo/RandomWalkSimulation.py
+++ monteCarlo/RandomWalkSimulation.py
@@ -38,16 +38,33 @@ simulated_prices = RandomWalkSimulation(num_simulations, num_periods, start_pric
 # Calculate the VaR at a 95% confidence level
 var = np.percentile(simulated_prices[-1, :], 5)
 
+# Calculate the expected returns, annualized volatility, and Sharpe ratio
+expected_returns = np.mean(simulated_prices[-1, :]) / start_price - 1
+annualized_volatility = np.std(simulated_prices[-1, :] / start_price - 1) * np.sqrt(252)
+sharpe_ratio = expected_returns / annualized_volatility
+
 # Plot a histogram of the simulated prices
 plt.hist(simulated_prices[-1, :], bins=50)
-plt.axvline(x=var, color='r', linestyle='--', label='VaR at 95% Confidence Level')
-plt.title("Monte Carlo Simulation of "+ ticker + " for the last 15 years")
+plt.axvline(x=start_price, color='r', linestyle='--', label='Starting Price')
+plt.axvline(x=var, color='b', linestyle='--', label='VaR at 95% Confidence Level')
+plt.axvline(x=np.mean(simulated_prices[-1, :]), color='g', linestyle='--', label='Expected Price')
+plt.title("Monte Carlo Simulation of " + ticker + " for the last 15 years")
 plt.xlabel("Final Stock Price")
 plt.ylabel("Frequency")
 plt.legend()
 plt.show()
 
-
-
-
+# Print the risk analysis, valuation, earnings, and volatility metrics
+print("Risk Analysis:")
+print("VaR at 95% Confidence Level: ", round(var - start_price, 2))
+print("Expected Annual Return: ", round(expected_returns * 100, 2), "%")
+print("Annualized Volatility: ", round(annualized_volatility * 100, 2), "%")
+print("Sharpe Ratio: ", round(sharpe_ratio, 2))
+print("")
+print("Valuation:")
+print("Starting Price: ", round(start_price, 2))
+print("Expected Price: ", round(np.mean(simulated_prices[-1, :]), 2))
+print("")
+print("Earnings:")
+print("Earnings Growth Rate")
 
